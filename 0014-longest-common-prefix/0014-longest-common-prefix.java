a86c699eb4b6433e7da1e4f7167910786781f6d6
Md. Rashad Tanjim <47326039+RashadTanjim@users.noreply.github.com>
2024-05-11 06:14:56 +0600

    Time: 0 ms (100.00%), Space: 41.1 MB (83.05%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public String longestCommonPrefix(String[] strs) {
+
+        if(strs.length == 0){
+            return "";
+        }
+
+        String firstEliment = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+
+            while (strs[i].indexOf(firstEliment) != 0){
+                firstEliment = firstEliment.substring(0, firstEliment.length() - 1);
+            }
+        }
+        return firstEliment;
+    }
+}
