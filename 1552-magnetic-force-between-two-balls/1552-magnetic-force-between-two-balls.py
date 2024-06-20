4f6355a34d203bd8299e81c74c13b4b2450b7728
Md. Rashad Tanjim <47326039+RashadTanjim@users.noreply.github.com>
2024-06-20 14:49:20 +0600

    Time: 954 ms (29.91%), Space: 29.9 MB (99.15%) - LeetHub

---
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxDistance(self, position: List[int], m: int) -> int:
+        position.sort()
+        
+        def canPlaceBalls(min_force):
+            count = 1  # Place the first ball at the first position
+            last_position = position[0]
+            
+            for i in range(1, len(position)):
+                if position[i] - last_position >= min_force:
+                    count += 1
+                    last_position = position[i]
+                if count >= m:
+                    return True
+            
+            return False
+        
+        low, high = 1, position[-1] - position[0]
+        best = 0
+        
+        while low <= high:
+            mid = (low + high) // 2
+            if canPlaceBalls(mid):
+                best = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        return best
