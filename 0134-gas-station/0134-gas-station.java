b8c50cf2d05890dff49effef74fec38516f79907
Md. Rashad Tanjim <47326039+RashadTanjim@users.noreply.github.com>
2025-01-24 20:22:31 +0600

    0134-GAS-STATION Stats: Time: 2 ms (97.72%), Space: 56.6 MB (16.11%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -2,7 +2,10 @@ class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) { // using gready approach
 
         int totalGas = 0, totalCost = 0;
-        for (int i = 0; i < gas.length; i++) {
+        int total = 0, start = 0;
+        int n = gas.length;
+
+        for (int i = 0; i < n; i++) {
             totalGas += gas[i];
             totalCost += cost[i];
         }
@@ -11,8 +14,7 @@ class Solution {
             return -1;
         }
 
-        int total = 0, start = 0;
-        for (int i = 0; i < gas.length; i++) {
+        for (int i = 0; i < n; i++) {
             total += gas[i] - cost[i];
 
             if (total < 0) {
