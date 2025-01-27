5573de00b44d39f993838971a6a04d9fc1357987
Md. Rashad Tanjim <47326039+RashadTanjim@users.noreply.github.com>
2025-01-27 22:12:16 +0600

    0058-LENGTH-OF-LAST-WORD Stats: Time: 1 ms (33.69%), Space: 41.9 MB (32.25%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,9 +1,29 @@
 class Solution {
     public int lengthOfLastWord(String s) {
 
-        var arr = s.split(" ");
-        String lastWord = arr[arr.length - 1];
+        int length = 0;
+        boolean count = false;
 
-        return lastWord.length();
+        for (char c : s.toCharArray()) {
+
+            if (c != ' ') {
+
+                if (!count) {
+
+                    count = true;
+                    length = 1;
+                } else {
+                    length++;
+                }
+            } else {
+                count = false;
+            }
+        }
+
+        return length;
+
+        // var arr = s.split(" ");
+        // String lastWord = arr[arr.length - 1];
+        // return lastWord.length();
     }
 }
