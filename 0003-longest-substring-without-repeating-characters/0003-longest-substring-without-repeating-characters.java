b5bc386e6ba09b1f64c37dac033a9b3951726e6a
Md. Rashad Tanjim <47326039+RashadTanjim@users.noreply.github.com>
2025-02-01 21:02:29 +0600

    0003-LONGEST-SUBSTRING-WITHOUT-REPEATING-CHARACTERS Stats: Time: 2 ms (98.51%), Space: 43.5 MB (91.49%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,18 +1,33 @@
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
+    public int lengthOfLongestSubstring(String s) { // Sliding Window + Hash Table (ASCII Array)
 
-        int left = 0, maxLength = 0;
-        HashSet<Character> charSet = new HashSet<>();
+        // approach 2
+        // int left = 0, maxLength = 0;
+        // HashSet<Character> charSet = new HashSet<>();
+
+        // for (int right = 0; right < s.length(); right++) {
+
+        // while (charSet.contains(s.charAt(right))) {
+        // charSet.remove(s.charAt(left));
+        // left++;
+        // }
+
+        // charSet.add(s.charAt(right));
+        // maxLength = Math.max(maxLength, right - left + 1);
+        // }
+        // return maxLength;
+
+        // approach 2
+
+        int[] charIndex = new int[128]; // ASCII characters
+        int maxLength = 0, left = 0;
 
         for (int right = 0; right < s.length(); right++) {
 
-            while (charSet.contains(s.charAt(right))) {
-                charSet.remove(s.charAt(left));
-                left++;
-            }
-
-            charSet.add(s.charAt(right));
+            left = Math.max(left, charIndex[s.charAt(right)]);
             maxLength = Math.max(maxLength, right - left + 1);
+
+            charIndex[s.charAt(right)] = right + 1;
         }
 
         return maxLength;
