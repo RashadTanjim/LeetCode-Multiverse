4874b534cac42d886f38d2e3123767cca13714c6
Md. Rashad Tanjim <47326039+RashadTanjim@users.noreply.github.com>
2025-02-06 03:12:59 +0600

    0036-VALID-SUDOKU Stats: Time: 1 ms (100%), Space: 44.4 MB (77.87%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,7 +1,9 @@
 public class Solution {
 
     public static boolean isValidSudoku(char[][] board) {
-        HashSet<String> seen = new HashSet<>();
+        int[] rows = new int[9]; // Track numbers in rows
+        int[] cols = new int[9]; // Track numbers in columns
+        int[] boxes = new int[9]; // Track numbers in 3x3 boxes
 
         for (int i = 0; i < 9; i++) {
 
@@ -9,11 +11,18 @@ public class Solution {
                 char num = board[i][j];
 
                 if (num != '.') {
-                    if (!seen.add(num + " in row " + i) ||
-                            !seen.add(num + " in col " + j) ||
-                            !seen.add(num + " in box " + (i / 3) + "-" + (j / 3))) {
-                        return false;
+                    int val = num - '1'; // Convert '1'-'9' to 0-8
+                    int boxIndex = (i / 3) * 3 + (j / 3);
+
+                    int mask = 1 << val; // Bit mask for current number
+                    if ((rows[i] & mask) != 0 || (cols[j] & mask) != 0 || (boxes[boxIndex] & mask) != 0) {
+                        return false; // Duplicate detected
                     }
+
+                    // Mark the number in row, column, and box
+                    rows[i] |= mask;
+                    cols[j] |= mask;
+                    boxes[boxIndex] |= mask;
                 }
             }
         }
