10efafc543a53d8a3dd007b975467c9fb72a2940
Md. Rashad Tanjim <47326039+RashadTanjim@users.noreply.github.com>
2025-02-06 03:11:50 +0600

    0036-VALID-SUDOKU Stats: Time: 14 ms (18.18%), Space: 44.5 MB (67.19%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,41 +1,19 @@
 public class Solution {
 
-    public boolean isValidSudoku(char[][] board) {
+    public static boolean isValidSudoku(char[][] board) {
+        HashSet<String> seen = new HashSet<>();
 
-        // Check rows and columns
         for (int i = 0; i < 9; i++) {
-            if (!isValidBlock(board, i, 0, i, 8) || !isValidBlock(board, 0, i, 8, i)) {
-                return false;
-            }
-        }
 
-        // Check each 3x3 sub-box
-        for (int row = 0; row < 9; row += 3) {
-
-            for (int col = 0; col < 9; col += 3) {
-                if (!isValidBlock(board, row, col, row + 2, col + 2)) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
-
-    private static boolean isValidBlock(char[][] board, int rowStart, int colStart, int rowEnd, int colEnd) {
-        boolean[] seen = new boolean[9]; // To store digits 1-9
-
-        for (int i = rowStart; i <= rowEnd; i++) {
-            for (int j = colStart; j <= colEnd; j++) {
+            for (int j = 0; j < 9; j++) {
                 char num = board[i][j];
 
                 if (num != '.') {
-                    int index = num - '1';
-
-                    if (seen[index])
+                    if (!seen.add(num + " in row " + i) ||
+                            !seen.add(num + " in col " + j) ||
+                            !seen.add(num + " in box " + (i / 3) + "-" + (j / 3))) {
                         return false;
-
-                    seen[index] = true;
+                    }
                 }
             }
         }
