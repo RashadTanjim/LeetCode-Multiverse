52f342d93708eae500d7c9b4b34ab2b4e7bc0fa7
Md. Rashad Tanjim <47326039+RashadTanjim@users.noreply.github.com>
2025-02-10 00:22:44 +0600

    0383-RANSOM-NOTE Stats: Time: 1 ms (99.15%), Space: 44.7 MB (77.18%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,19 +1,40 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
 
-        HashMap<Character, Integer> map = new HashMap<>();
-
+        // Count occurrences of characters in the magazine
         for (char c : magazine.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
+            count[c - 'a']++;
         }
 
+        // Check if we can construct the ransomNote from the magazine
         for (char c : ransomNote.toCharArray()) {
-            if (!map.containsKey(c) || map.get(c) <= 0) {
-                return false;
+            if (count[c - 'a'] == 0) {
+                return false; // Not enough characters in magazine
             }
-            map.put(c, map.get(c) - 1);
+            count[c - 'a']--; 
         }
 
         return true;
     }
 }
+
+// class Solution {
+// public boolean canConstruct(String ransomNote, String magazine) {
+
+// HashMap<Character, Integer> map = new HashMap<>();
+
+// for (char c : magazine.toCharArray()) {
+// map.put(c, map.getOrDefault(c, 0) + 1);
+// }
+
+// for (char c : ransomNote.toCharArray()) {
+// if (!map.containsKey(c) || map.get(c) <= 0) {
+// return false;
+// }
+// map.put(c, map.get(c) - 1);
+// }
+
+// return true;
+// }
+// }
