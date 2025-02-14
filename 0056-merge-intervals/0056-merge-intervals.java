d1e5e9b1e78c01d9ab00bb1cb4263efe790a823e
Md. Rashad Tanjim <47326039+RashadTanjim@users.noreply.github.com>
2025-02-15 04:11:33 +0600

    0056-MERGE-INTERVALS Stats: Time: 9 ms (49.53%), Space: 46.4 MB (91.06%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,22 +1,19 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
 
-        if (intervals.length <= 1)
-            return intervals;
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        int index = 0;
 
-        Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
+        for (int i = 1; i < intervals.length; i++) {
 
-        LinkedList<int[]> merged = new LinkedList<>();
-
-        for (int[] interval : intervals) {
-
-            if (merged.isEmpty() || merged.getLast()[1] < interval[0]) {
-                merged.add(interval);
+            if (intervals[index][1] >= intervals[i][0]) {
+                intervals[index][1] = Math.max(intervals[index][1], intervals[i][1]);
             } else {
-                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
+                index++;
+                intervals[index] = intervals[i];
             }
         }
 
-        return merged.toArray(new int[merged.size()][]);
+        return Arrays.copyOfRange(intervals, 0, index + 1);
     }
 }
