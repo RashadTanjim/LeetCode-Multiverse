35af51ccab5f489af5712119c749c91c049ca0dd
Md. Rashad Tanjim <47326039+RashadTanjim@users.noreply.github.com>
2025-02-28 03:44:33 +0600

    0146-LRU-CACHE Stats: Time: 53 ms (22.06%), Space: 114.9 MB (51.32%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,23 +1,68 @@
-class LRUCache extends LinkedHashMap<Integer, Integer> {
+class LRUCache {
+
+    private class Node {
+        int key, value;
+        Node prev, next;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
 
     private int capacity;
+    private Map<Integer, Node> cache;
+    private Node head, tail;
 
     public LRUCache(int capacity) {
-        super(capacity, 0.75f, true);
         this.capacity = capacity;
+        this.cache = new HashMap<>();
+
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head.next = tail;
+        tail.prev = head;
     }
 
     public int get(int key) {
-        return super.getOrDefault(key, -1);
+
+        if (!cache.containsKey(key)) {
+            return -1;
+        }
+
+        Node node = cache.get(key);
+        remove(node);
+        insertAtFront(node);
+
+        return node.value;
     }
 
     public void put(int key, int value) {
-        super.put(key, value);
+
+        if (cache.containsKey(key)) {
+            remove(cache.get(key));
+        }
+
+        if (cache.size() == capacity) {
+            remove(tail.prev);
+        }
+
+        insertAtFront(new Node(key, value));
     }
 
-    @Override
-    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
-        return size() > capacity;
+    private void remove(Node node) {
+        cache.remove(node.key);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void insertAtFront(Node node) {
+        cache.put(node.key, node);
+
+        node.next = head.next;
+        node.prev = head;
+        head.next.prev = node;
+        head.next = node;
     }
 }
 
