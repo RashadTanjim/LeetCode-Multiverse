50b2bcd498c3056687a210eb2a45de06302c47f6
Md. Rashad Tanjim <47326039+RashadTanjim@users.noreply.github.com>
2025-03-15 03:29:50 +0600

    0127-WORD-LADDER Stats: Time: 89 ms (51.03%), Space: 47.7 MB (31.93%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> wordSet = new HashSet<>(wordList);
+
+        if (!wordSet.contains(endWord))
+            return 0;
+
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        int level = 1;
+
+        Set<String> visited = new HashSet<>();
+        visited.add(beginWord);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                String currentWord = queue.poll();
+                char[] wordArray = currentWord.toCharArray();
+
+                for (int j = 0; j < wordArray.length; j++) {
+                    char originalChar = wordArray[j];
+
+                    // Try all letters from 'a' to 'z'
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        wordArray[j] = c;
+                        String newWord = new String(wordArray);
+
+                        // If newWord is in the other set, we have found a path
+                        if (newWord.equals(endWord)) {
+                            return level + 1;
+                        }
+
+                        // If newWord exists in wordSet and hasn't been visited
+                        if (wordSet.contains(newWord) && !visited.contains(newWord)) {
+                            queue.offer(newWord);
+                            visited.add(newWord);
+                        }
+                    }
+
+                    wordArray[j] = originalChar; // Restore the original character
+                }
+            }
+            level++;
+        }
+
+        return 0;
+    }
+}
