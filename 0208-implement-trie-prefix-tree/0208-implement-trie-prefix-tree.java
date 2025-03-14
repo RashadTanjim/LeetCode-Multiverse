04b0f3cee9e3ab22f86263200eefbdd0504b217e
Md. Rashad Tanjim <47326039+RashadTanjim@users.noreply.github.com>
2025-03-15 05:02:44 +0600

    0208-IMPLEMENT-TRIE-PREFIX-TREE Stats: Time: 37 ms (51.32%), Space: 55.6 MB (35.05%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,28 +1,24 @@
-import java.util.*;
-
-public class TrieNode {
-    public Map<Character, TrieNode> children;
-    public boolean isWord;
-
-    public TrieNode() {
-        this.children = new HashMap<>();
-        this.isWord = false;
-    }
+class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    boolean isWord;
 }
 
-class Trie { // HashMap Based
-    private final TrieNode root;
+class Trie { // Array Based - fixed size
+    private TrieNode root;
 
     public Trie() {
-        this.root = new TrieNode();
+        root = new TrieNode();
     }
 
     public void insert(String word) {
         TrieNode node = root;
 
         for (char c : word.toCharArray()) {
-            node.children.putIfAbsent(c, new TrieNode());
-            node = node.children.get(c);
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new TrieNode();
+            }
+            node = node.children[index];
         }
         node.isWord = true;
     }
@@ -38,21 +34,14 @@ class Trie { // HashMap Based
 
     private TrieNode findNode(String prefix) {
         TrieNode node = root;
-
+        
         for (char c : prefix.toCharArray()) {
-            if (!node.children.containsKey(c)) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
                 return null;
             }
-            node = node.children.get(c);
+            node = node.children[index];
         }
         return node;
     }
 }
-
-/**
- * Your Trie object will be instantiated and called as such:
- * Trie obj = new Trie();
- * obj.insert(word);
- * boolean param_2 = obj.search(word);
- * boolean param_3 = obj.startsWith(prefix);
- */
