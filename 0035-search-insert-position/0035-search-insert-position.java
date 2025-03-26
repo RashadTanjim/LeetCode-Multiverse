7b2f794596ecdc3fd254963de3d1d8ed8160677e
Md. Rashad Tanjim <47326039+RashadTanjim@users.noreply.github.com>
2025-03-26 22:02:32 +0600

    0035-SEARCH-INSERT-POSITION Stats: Time: 0 ms (100%), Space: 42.9 MB (65.68%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,10 +1,19 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] >= target)
-                return i;
+        int low = 0, high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
 
-        return nums.length;
+        return low;
     }
 }
