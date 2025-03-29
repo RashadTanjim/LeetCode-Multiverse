3a53bc89d32cfbe573648ce2a8b97a93c1b2966c
Md. Rashad Tanjim <47326039+RashadTanjim@users.noreply.github.com>
2025-03-30 01:33:50 +0600

    0502-IPO Stats: Time: 164 ms (14.03%), Space: 63.8 MB (38.88%) - LeetHub

--- 0502-ipo/0502-ipo.java
+++ 0502-ipo/0502-ipo.java
@@ -1,63 +1,32 @@
 class Solution {
-    
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-        
         int n = profits.length;
-        Project[] projects = new Project[n];
 
-        // Create project objects and sort by capital needed
+        PriorityQueue<Project> minCapitalHeap = new PriorityQueue<>(Comparator.comparingInt(a -> a.capital));
+        PriorityQueue<Integer> maxProfitHeap = new PriorityQueue<>(Collections.reverseOrder());
+
         for (int i = 0; i < n; i++) {
-            projects[i] = new Project(capital[i], profits[i]);
+            minCapitalHeap.offer(new Project(capital[i], profits[i]));
         }
-        
-        Arrays.sort(projects, Comparator.comparingInt(p -> p.capital));
 
-        MaxHeap maxHeap = new MaxHeap();
-        int projectIndex = 0;
-
+        // Perform up to k projects
         for (int i = 0; i < k; i++) {
-            
-            // Add all projects that can be started with current capital
-            while (projectIndex < n && projects[projectIndex].capital <= w) {
-                maxHeap.push(projects[projectIndex].profit);
-                projectIndex++;
+
+            // Add all projects we can afford to maxProfitHeap
+            while (!minCapitalHeap.isEmpty() && minCapitalHeap.peek().capital <= w) {
+                maxProfitHeap.offer(minCapitalHeap.poll().profit);
             }
 
-            // If no projects can be started, break early
-            if (maxHeap.isEmpty()) {
+            if (maxProfitHeap.isEmpty()) {
                 break;
             }
 
-            // Choose the most profitable project
-            w += maxHeap.pop();
+            w += maxProfitHeap.poll();
         }
 
         return w;
     }
-    
-        
-    // Max heap implementation using PriorityQueue
-    private static class MaxHeap {
-        
-        private PriorityQueue<Integer> heap;
 
-        public MaxHeap() {
-            heap = new PriorityQueue<>(Collections.reverseOrder());
-        }
-
-        public void push(int val) {
-            heap.add(val);
-        }
-
-        public int pop() {
-            return heap.poll();
-        }
-
-        public boolean isEmpty() {
-            return heap.isEmpty();
-        }
-    }
-    
     private static class Project {
         int capital;
         int profit;
@@ -67,5 +36,4 @@ class Solution {
             this.profit = profit;
         }
     }
-
 }
