a2cd2ecaed8a1d53602b31d311e17d54e611e2d1
Md. Rashad Tanjim <47326039+RashadTanjim@users.noreply.github.com>
2025-04-21 22:09:08 +0600

    0437-PATH-SUM-III Stats: Time: 3 ms (96.68%), Space: 44.5 MB (25.03%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -15,29 +15,25 @@
  */
 class Solution {
     public int pathSum(TreeNode root, int targetSum) {
-        HashMap<Long, Integer> prefixSumCount = new HashMap<>();
-        prefixSumCount.put(0L, 1); // Base case for prefix sum
+        HashMap<Long, Integer> prefixSum = new HashMap<>();
+        prefixSum.put(0L, 1); // Base case for prefix sum
 
-        return dfs(root, 0L, targetSum, prefixSumCount);
+        return dfs(root, 0L, targetSum, prefixSum);
     }
 
-    private int dfs(TreeNode node, long currentSum, int targetSum, HashMap<Long, Integer> prefixSumCount) {
+    private int dfs(TreeNode node, long currentSum, int targetSum, HashMap<Long, Integer> prefixSum) {
         if (node == null)
             return 0;
 
         currentSum += node.val;
-        int count = prefixSumCount.getOrDefault(currentSum - targetSum, 0);
+        int count = prefixSum.getOrDefault(currentSum - targetSum, 0);
 
-        prefixSumCount.put(currentSum, prefixSumCount.getOrDefault(currentSum, 0) + 1);
+        prefixSum.put(currentSum, prefixSum.getOrDefault(currentSum, 0) + 1);
 
-        count += dfs(node.left, currentSum, targetSum, prefixSumCount);
-        count += dfs(node.right, currentSum, targetSum, prefixSumCount);
+        count += dfs(node.left, currentSum, targetSum, prefixSum);
+        count += dfs(node.right, currentSum, targetSum, prefixSum);
 
-        prefixSumCount.put(currentSum, prefixSumCount.get(currentSum) - 1);
-        
-        if (prefixSumCount.get(currentSum) == 0) {
-            prefixSumCount.remove(currentSum);
-        }
+        prefixSum.put(currentSum, prefixSum.get(currentSum) - 1);
 
         return count;
     }
