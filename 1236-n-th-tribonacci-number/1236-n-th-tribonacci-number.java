0f7311410f2261d2554a8ed1737d323b3e10e21e
Md. Rashad Tanjim <47326039+RashadTanjim@users.noreply.github.com>
2025-06-17 01:37:40 +0600

    1236-N-TH-TRIBONACCI-NUMBER Stats: Time: 0 ms (100%), Space: 40.2 MB (80.51%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
@@ -1,11 +1,19 @@
 class Solution {
     public int tribonacci(int n) {
-        int[] dp = { 0, 1, 1 };
+        if (n == 0)
+            return 0;
+        if (n == 1 || n == 2)
+            return 1;
+
+        int t0 = 0, t1 = 1, t2 = 1, tn = 0;
 
         for (int i = 3; i <= n; i++) {
-            dp[i % 3] = dp[0] + dp[1] + dp[2];
+            tn = t0 + t1 + t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = tn;
         }
 
-        return dp[n % 3];
+        return tn;
     }
 }
